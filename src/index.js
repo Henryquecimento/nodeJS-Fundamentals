6106deae7066ce8887f61e2405492ffebf26e9e4
henryque <henryque_1@hotmail.com>
2021-11-02 13:12:54 -0300

    fix: readjust delete route

--- src/index.js
+++ src/index.js
@@ -2,23 +2,45 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/courses", (req, res) => {
+  const query = req.query;
+
+  console.log(query);
+
   return res.json(["Curso_1", "Curso_2", "Curso_3"]);
 });
 
 app.post("/courses", (req, res) => {
+  const body = req.body;
+
+  console.log(body);
+
   return res.json(["Curso_1", "Curso_2", "Curso_3", "Curso_4"]);
 });
 
 app.put('/courses/:id', (req, res) => {
+  const { id } = req.params;
+
+  console.log(id);
+
   return res.json(["Curso_5", "Curso_2", "Curso_3", "Curso_4"]);
 });
 
 app.patch('/courses/:id', (req, res) => {
+  const { id } = req.params;
+
+  console.log(id);
+
   return res.json(["Curso_5", "Curso_6", "Curso_3", "Curso_4"]);
 })
 
-app.delete('/couses/:id', (req, res) => {
+app.delete('/courses/:id', (req, res) => {
+  const { id } = req.params;
+
+  console.log(id);
+
   return res.json(["Curso_5", "Curso_6"]);
 })
 
